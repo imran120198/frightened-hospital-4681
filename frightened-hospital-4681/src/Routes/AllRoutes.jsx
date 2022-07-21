cd06e835fe0e6ca7a835fa14d025e7d1831d9709
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-07-21 17:29:56 +0530

    footer added

--- frightened-hospital-4681/src/Routes/AllRoutes.jsx
+++ frightened-hospital-4681/src/Routes/AllRoutes.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Categories from "../Components/Categoris/Categories";
-import CatNavbar from "../Components/Categoris/CatNavbar";
 import Navbar from "../Components/Navbar/Navbar";
-import TopSlider from "../Pages/Carousel/TopSlider";
 import Home from "../Pages/Home/Home";
-import MultipleItems from "../Pages/Carousel/TopSlider";
+import Footer from "../Components/Footer/Footer";
 
 const AllRoutes = () => {
   return (
@@ -12,7 +10,7 @@ const AllRoutes = () => {
       <Navbar />
       <Categories />
       <Home />
-      {/* <CatNavbar /> */}
+      <Footer/>
       
     </div>
   );
